667025ea930b2a251f5002b46ff808fa75af4ba3
evgen <koles.doc96.09.27@gmail.com>
2020-12-08 22:22:18 +0700

    hometask_2 complete2

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -27,26 +27,61 @@ fun Date.add(vaue: Int, units: TimeUnits): Date {
 }
 
 fun Date.humanizeDiff(date: Date = Date()): String {
-    val humanizeDate: String
-    val currentDate = date
-    val period = currentDate.time - this.time
-    val seconds = period / SECOND
-    humanizeDate = when {
-        seconds in 0..1 -> "только что"
-        seconds in 2..45 -> "несколько секунд назад"
-        seconds in 46..75 -> "минуту назад"
-        seconds in 76..45 * MINUTE / SECOND -> "${TimeUnits.MINUTE.plural(  period / MINUTE)} назад"
-        seconds in 46 * MINUTE / SECOND..75 * MINUTE / SECOND -> "час назад"
-        seconds in 75 * MINUTE / SECOND..22 * HOUR / SECOND -> "${TimeUnits.HOUR.plural(period / HOUR)} назад"
-        seconds in 22 * HOUR / SECOND..26 * HOUR / SECOND -> "день назад"
-        seconds in 26 * HOUR / SECOND..360 * DAY / SECOND -> "${TimeUnits.DAY.plural(period / DAY)} назад"
-        else -> "более года назад"
-    }
+//    val humanizeDate: String
+//    val currentDate = date
+//    val period = currentDate.time - this.time
+//    val seconds = period / SECOND
+//    humanizeDate = when {
+//        seconds in 0..1 -> "только что"
+//        seconds in 2..45 -> "несколько секунд назад"
+//        seconds in 46..75 -> "минуту назад"
+//        seconds in 76..45 * MINUTE / SECOND -> "${TimeUnits.MINUTE.plural(  period / MINUTE)} назад"
+//        seconds in 46 * MINUTE / SECOND..75 * MINUTE / SECOND -> "час назад"
+//        seconds in 75 * MINUTE / SECOND..22 * HOUR / SECOND -> "${TimeUnits.HOUR.plural(period / HOUR)} назад"
+//        seconds in 22 * HOUR / SECOND..26 * HOUR / SECOND -> "день назад"
+//        seconds in 26 * HOUR / SECOND..360 * DAY / SECOND -> "${TimeUnits.DAY.plural(period / DAY)} назад"
+//        else -> "более года назад"
+//    }
 
-    return humanizeDate
+    val now = Date()
+    val isFuture: Boolean
+
+    // разница в мс
+    var diff = now.time - this.time
+    isFuture = diff <= 0
+    diff = Math.abs(diff)
+
+
+    if (isFuture) {
+        // будущее
+        return when (diff) {
+            (0 * SECOND), (1 * SECOND) -> "через секунду"
+            in (1 * SECOND)..(45 * SECOND) -> "через несколько секунд"
+            in (45 * SECOND)..(75 * SECOND) -> "через минуту"
+            in (75 * SECOND)..(45 * MINUTE) -> "через ${TimeUnits.MINUTE.plural(diff / MINUTE)}"
+            in (45 * MINUTE)..(75 * MINUTE) -> "через час"
+            in (75 * MINUTE)..(22 * HOUR) -> "через ${TimeUnits.HOUR.plural(diff / HOUR)}"
+            in (22 * HOUR)..(26 * HOUR) -> "через день"
+            in (26 * HOUR)..(360 * DAY) -> "через ${TimeUnits.DAY.plural(diff / DAY)}"
+            else -> "более чем через год"
+        }
+
+    } else {
+        return when (diff) {
+            (0 * SECOND), (1 * SECOND) -> "только что"
+            in (1 * SECOND)..(45 * SECOND) -> "несколько секунд назад"
+            in (45 * SECOND)..(75 * SECOND) -> "минуту назад"
+            in (75 * SECOND)..(45 * MINUTE) -> "${TimeUnits.MINUTE.plural(diff / MINUTE)} назад"
+            in (45 * MINUTE)..(75 * MINUTE) -> "час назад"
+            in (75 * MINUTE)..(22 * HOUR) -> "${TimeUnits.HOUR.plural(diff / HOUR)} назад"
+            in (22 * HOUR)..(26 * HOUR) -> "день назад"
+            in (26 * HOUR)..(360 * DAY) -> "${TimeUnits.DAY.plural(diff / DAY)} назад"
+            else -> "более года назад"
+        }
+    }
 }
 
-enum class TimeUnits  {
+enum class TimeUnits {
     SECOND {
         override fun plural(value: Long): String {
             return "$value ${Utils.plural(value.toInt(), Utils.WordDeclension.SECOND)}"
@@ -67,5 +102,6 @@ enum class TimeUnits  {
             return "$value ${Utils.plural(value.toInt(), Utils.WordDeclension.DAY)}"
         }
     };
-    abstract fun plural(value: Long):String
+
+    abstract fun plural(value: Long): String
 }
